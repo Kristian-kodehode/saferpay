9542cd146f6bb12f64ee9770802de54b19d5588a
Kristian Kodehode <kristian007.kodehode@gmail.com>
2023-10-05 14:14:51 +0200

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <>
+      <div className="headingdiv">
+        <h1>Welcome to Saferpay</h1>
+        <h4>
+          With state-of-the-art encryption and fraud protection, SaferPay is
+          your trusted partner for safe and seamless transactions.
+        </h4>
+      </div>
+    </>
+  );
+}
+
+export default App;
