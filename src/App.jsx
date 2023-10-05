4aac5f44ed811adcc46b9c8bec33de99368899f9
Kristian Kodehode <kristian007.kodehode@gmail.com>
2023-10-05 14:45:03 +0200

    adjusting main

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.scss";
+import illustration from "./images/illustrationGirlAndDog.png";
 
 function App() {
   return (
@@ -9,14 +10,22 @@ function App() {
           <li>item</li>
           <li>item</li>
         </nav>
-        <div className="headingdiv">
-          <h1>Welcome to Saferpay</h1>
-          <h4>
-            With state-of-the-art encryption and fraud protection, SaferPay is
-            your trusted partner for safe and seamless transactions.
-          </h4>
-          <button className="button-seondary">Read more</button>
-        </div>
+        <main className="container">
+          <div className="headingdiv">
+            <h1>Welcome to Saferpay</h1>
+            <h4>
+              With state-of-the-art encryption and fraud protection, SaferPay is
+              your trusted partner for safe and seamless transactions.
+            </h4>
+            <button className="button-seondary">Read more</button>
+          </div>
+          <div>
+            <img
+              src={illustration}
+              alt=""
+            />
+          </div>
+        </main>
       </div>
     </>
   );
