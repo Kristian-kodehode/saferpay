c2ce04e90c9a72297cece990d0abe50c19a66149
Kristian Kodehode <kristian007.kodehode@gmail.com>
2023-10-05 14:30:59 +0200

    styling margin bottomon h4 button

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,20 @@ import "./App.scss";
 function App() {
   return (
     <>
-      <div className="headingdiv">
+      <div>
         <nav>
           <li>item</li>
           <li>item</li>
           <li>item</li>
         </nav>
-        <h1>Welcome to Saferpay</h1>
-        <h4>
-          With state-of-the-art encryption and fraud protection, SaferPay is
-          your trusted partner for safe and seamless transactions.
-        </h4>
+        <div className="headingdiv">
+          <h1>Welcome to Saferpay</h1>
+          <h4>
+            With state-of-the-art encryption and fraud protection, SaferPay is
+            your trusted partner for safe and seamless transactions.
+          </h4>
+          <button className="button-seondary">Read more</button>
+        </div>
       </div>
     </>
   );
