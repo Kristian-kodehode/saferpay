48433bde5d588afaeec3218fbdf67d204eaf3b16
Kristian Kodehode <kristian007.kodehode@gmail.com>
2023-10-09 11:01:53 +0200

    working with navbar fx

--- src/Sections/NavSection.tsx
+++ src/Sections/NavSection.tsx
@@ -1,13 +1,37 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import logoNav from "../images/logoNav.svg";
 
 const NavSection = () => {
   const [isMenuOpen, setMenuOpen] = useState(false);
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
 
   const toggleMenu = () => {
     setMenuOpen(!isMenuOpen);
     console.log("burger clicked");
   };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.scrollY;
+      const navElement = document.querySelector("nav");
+
+      if (navElement) {
+        if (prevScrollPos > currentScrollPos) {
+          navElement.style.top = "0";
+        } else {
+          navElement.style.top = "-100px";
+        }
+      }
+
+      setPrevScrollPos(currentScrollPos);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [prevScrollPos]);
 
   return (
     <div>
