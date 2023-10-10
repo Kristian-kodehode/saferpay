cdebd075910b2291776b5a1fa7d0606c7dcbc1e2
Kristian Kodehode <kristian007.kodehode@gmail.com>
2023-10-10 14:02:26 +0200

    added icons and some extra detail styling

--- src/Sections/OurGuarantee.tsx
+++ src/Sections/OurGuarantee.tsx
@@ -6,10 +6,12 @@ const OurGuarantee = () => {
       <h3>Our guarantee</h3>
       <div className="guarantee-cards-container">
         <div className="guarantee-card">
+          <i className="fa-solid fa-user-shield"></i>
           <h6>guarantee</h6>
           <h3>We got your back when the scammers are loose.</h3>
         </div>
         <div className="guarantee-card">
+          <i className="fa-solid fa-lock"></i>
           <h6>guarantee</h6>
           <h3>State of the art encryption for your protection.</h3>
         </div>
