daccf9fac65bdff6ac5afc6f4f51b13331be82f1
Kristian Kodehode <kristian007.kodehode@gmail.com>
2023-10-11 12:12:51 +0200

    updated guarantee section. removed a section

--- src/Sections/OurGuarantee.tsx
+++ src/Sections/OurGuarantee.tsx
@@ -4,27 +4,26 @@ import IllustrationDeal from "../images/IllustrationDeal.png";
 const OurGuarantee: React.FC = () => {
   return (
     <section id="our-guarantee">
-      <section>
-        <h3 className="section-heading">Our guarantee</h3>
-        <div className="guarantee-cards-container">
-          <div className="guarantee-card">
-            <i className="fa-solid fa-user-shield"></i>
-            <h6>guarantee</h6>
-            <h3>We got your back when the scammers are loose.</h3>
-          </div>
-          <div className="guarantee-card">
-            <i className="fa-solid fa-lock"></i>
-            <h6>guarantee</h6>
-            <h3>State of the art encryption for your protection.</h3>
-          </div>
-          <div>
-            <img
-              src={IllustrationDeal}
-              alt="illustration of two hands shaking eachother"
-            />
-          </div>
+      <h3 className="section-heading">Our guarantee</h3>
+      <div className="guarantee-cards-container">
+        <div className="guarantee-card">
+          <i className="fa-solid fa-user-shield"></i>
+          <h6>guarantee</h6>
+          <h3>We got your back when the scammers are loose.</h3>
         </div>
-      </section>
+        <div className="guarantee-card">
+          <i className="fa-solid fa-lock"></i>
+          <h6>guarantee</h6>
+          <h3>State of the art encryption for your protection.</h3>
+        </div>
+        <div>
+          <img
+            src={IllustrationDeal}
+            alt="illustration of two hands shaking eachother"
+          />
+        </div>
+      </div>
+
       <section>
         <h3 className="section-heading">Seriously good</h3>
         <div className="guarantee-cards-container">
